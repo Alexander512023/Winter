28fe98f8e92782348853939841aed06495f18613
Alex Goryanin <goryaninaa@icloud.com>
2023-03-17 16:19:18 +0400

    Suppress unused

--- src/main/java/com/goryaninaa/winter/cache/CacheStorage.java
+++ src/main/java/com/goryaninaa/winter/cache/CacheStorage.java
@@ -14,6 +14,7 @@ import java.util.concurrent.Future;
  * @author Alex Goryanin
  * @param <V> - entity object, which will be cached
  */
+@SuppressWarnings("unused")
 public class CacheStorage<V> implements Cache<V> {
 
   private final Storage<V> storage;
